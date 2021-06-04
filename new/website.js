5026ef1d189acc81501e6b0f122d9d3e80a8c703
dolljm <dolljm@miamioh.edu>
2021-06-04 00:20:55 -0700

    minor changes

--- new/website.js
+++ new/website.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    setInterval(rotator, 2000);
+    setInterval(rotator, 3000);
 });
 
 function fader() {
@@ -7,7 +7,7 @@ function fader() {
 }
 
 function rotator() {   
-    var words = new Array("run.", "play tennis.", "be myself.", "stay up late.", "workout.", "swim.", "sleep in.", "play fantasy baseball.", "code.", "have fun.", "be outside.", "play video games.", "compete.", "play baseball.", "fall asleep to thunderstorms.", "watch sports.", "make brackets for everything.", "play hockey.", "skate.", "make videos.", "be a creator.", "invent.", "be with my friends.", "watch the sunset.", "make things better.", "engineer.", "create websites.", "learn.", "catch up with friends.", "work on my computer.", "program.", "challenge myself.", "improve.", "be happy.", "spend time with family.", "make new things.", "innovate.");
+    var words = new Array("run.", "play tennis.", "be myself.", "stay up late.", "workout.", "swim.", "sleep in.", "play fantasy baseball.", "code.", "throw frisbee.","have fun.", "be outside.", "play video games.", "compete.", "play baseball.", "fall asleep to thunderstorms.", "watch sports.", "make brackets for everything.", "play hockey.", "skate.", "make videos.", "be a creator.", "invent.", "be with my friends.", "watch the sunset.", "make things better.", "engineer.", "create websites.", "learn.", "catch up with friends.", "work on my computer.", "program.", "challenge myself.", "improve.", "be happy.", "spend time with family.", "make new things.", "innovate.");
     var random = Math.floor(Math.random()*words.length);
     $("#word").text(words[random]);
 }
