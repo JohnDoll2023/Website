5a797e720666fe43494c9b9db20eec3493717be0
dolljm <dolljm@miamioh.edu>
2021-06-05 00:30:04 -0700

    updated js to not have same word in a row

--- new/website.js
+++ new/website.js
@@ -1,3 +1,5 @@
+var history;
+
 $(document).ready(function(){
     setInterval(rotator, 3000);
 });
@@ -9,6 +11,10 @@ function fader() {
 function rotator() {   
     var words = new Array("run.", "play tennis.", "be myself.", "stay up late.", "workout.", "swim.", "sleep in.", "play fantasy baseball.", "code.", "throw frisbee.","have fun.", "be outside.", "play video games.", "compete.", "play baseball.", "fall asleep to thunderstorms.", "watch sports.", "make brackets for everything.", "play hockey.", "skate.", "make videos.", "be a creator.", "invent.", "be with my friends.", "watch the sunset.", "make things better.", "engineer.", "create websites.", "learn.", "catch up with friends.", "work on my computer.", "program.", "challenge myself.", "improve.", "be happy.", "spend time with family.", "make new things.", "innovate.");
     var random = Math.floor(Math.random()*words.length);
+    while (random == history) {
+        random = Math.floor(Math.random()*words.length);
+    }
+    history = random;
     $("#word").text(words[random]);
 }
 
